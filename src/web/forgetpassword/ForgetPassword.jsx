f566157b750567d3a7ae219a77bc6fc2cec1853f
razan0r <razanhamad815@gmail.com>
2024-02-11 19:34:31 +0300

    edit pass

--- src/web/forgetpassword/ForgetPassword.jsx
+++ src/web/forgetpassword/ForgetPassword.jsx
@@ -4,6 +4,8 @@
   import { forgetSchema } from '../validation/validation.js';
   import axios from 'axios';
   import {  useNavigate } from 'react-router-dom';
+  import { toast } from 'react-toastify';
+
   function ForgetPassword() {
     const navigate=useNavigate();
       const initialValues={
@@ -12,12 +14,27 @@
           code:'',
       }
       
-      const onSubmit=async ({email,password,code})=>{
-
-           const {data} = await axios.patch(`${import.meta.env.VITE_API_URL}/auth/forgotPassword`,{email,password,code});            
-           navigate('/login')
+      const onSubmit= async users => {
+        try {
+          const {data} = await axios.patch("https://ecommerce-node4.vercel.app/auth/forgotPassword", users);
+          console.log('Response:', data);
+          if(data.message=='success'){
+              toast.success('password updated succecfully', {
+                  position: "top-right",
+                  autoClose: false,
+                  hideProgressBar: false,
+                  closeOnClick: true,
+                  pauseOnHover: true,
+                  draggable: true,
+                  progress: undefined,
+                  theme: "light",
+                  });
+              navigate('/login');
           }
-          
+        } catch (error) {
+          console.error('Error updating password:', error.message);
+        }
+      }
       
       const formik = useFormik({
         initialValues,
@@ -69,14 +86,14 @@
      <div className='container login-container  w-100  rounded-3 mx-3'>
      <div className='row justify-content-center py-5 align-items-center all all1'>
         <div className='col-md-5'>
-          <img src='login1.PNG' className='w-90'/>
+          <img src='photo-1590959651373-a3db0f38a961.avif' className='w-90'/>
           </div>
           <div className='col-md-4 '>
     
       <h2 className='login-h2 text-center'>Forget Password</h2>
       <form onSubmit={formik.handleSubmit}>
       {renderInput}
-      <button type='submit' className='submit' onClick={()=>onSubmit(formik.values)}>submit</button>
+      <button type='submit' className='submit' disabled={!formik.isValid} >submit</button>
       </form>
       </div> 
     </div>
